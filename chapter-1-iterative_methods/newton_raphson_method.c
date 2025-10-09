8e266c99428288e56110b99e90cd4cbcd78ace7a
Anush Shrestha <anush.stha980@outlook.com>
2025-10-09 20:30:35 +0545

    newton raphon algo added

--- chapter-1-iterative_methods/newton_raphson_method.c
+++ chapter-1-iterative_methods/newton_raphson_method.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-#define f(x) (x * x * x + x + 1) // x^3+x+1
-#define df(x) (3*x*x+1) //3X^2+1
+#define f(x) (x * x * x + x + 1); // x^3+x+1
+#define df(x) (3*x*x+1); //3X^2+1
+#define EPS 0.005;
 
 int main()
 {
@@ -10,17 +11,18 @@ int main()
 
     printf("\nEnter initial guesses (x0): ");
     scanf("%f", &x0);
-
-    printf("\nEnter the Error tolerance: ");
-    scanf("%f", &error);
  
-
     // main program
-    while (1)
-    {
-     x1 =x0 -f(x0)/df(x0);
-     error= fabs(x1-x0);
+    while(1){
+x1 = x0-f(x0)/df(x0);
+error = fabs(x1-x0);
+if (error<EPS)
+break;
+
+x1=x0;
     }
-    printf("\nThe approximate root is %.4f\n", xm);
+
+  
+    printf("\nThe approximate root is %.4f\n", x1);
     return 0;
 }
